79fa3d90abde7df10b0de88cd666ea9589896a36
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2020-09-14 17:14:48 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-
 /***************** Menu-btn ********************/
-let menu_burger = document.querySelector('.menu__burger');
+let menu_burger = document.querySelector('.header__burger');
 let menu = document.querySelector('.menu');
 let menu_lock = document.querySelector('body');
 menu_burger.addEventListener("click", function (e) {
 	menu.classList.toggle('active');
 	menu_burger.classList.toggle('active');
 	menu_lock.classList.toggle('lock');
-});
+}); 
+
 
 /***************** WebP ********************/
 function testWebP(callback) {
@@ -38,3 +38,55 @@ function ibg(){
 	}
 }
 ibg();
+$(function(){
+
+	/***************** nav-aside active ********************/
+	jQuery(window).scroll(function(){
+		var $sections = $('.articles__item');
+		$sections.each(function(i,el){
+	  var top  = $(el).offset().top-100;
+	  var bottom = top +$(el).height();
+	  var scroll = $(window).scrollTop();
+	  var id = $(el).attr('id');
+	 if( scroll > top && scroll < bottom){
+			$('a.nav-aside__list-link--active').removeClass('nav-aside__list-link--active');
+		$('a[href="#'+id+'"]').addClass('nav-aside__list-link--active');
+
+	  }
+ })
+});
+
+/***************** Scroll to ********************/
+$(".nav-aside__list-link").on("click","a", function (event) {
+	  event.preventDefault();
+	  var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		
+	  $('body,html').animate({scrollTop: top}, 100);
+ });
+});
+/***************** nav-aside-btn ********************/
+let nav_burger = document.querySelector('.nav-aside__burger');
+let content_knowledge = document.querySelector('.knowledge__body');
+let nav = document.querySelector('.nav-aside');
+nav_burger.addEventListener("click", function (e) {
+	nav.classList.toggle('active');
+	nav_burger.classList.toggle('active');
+	content_knowledge.classList.toggle('active');
+});
+
+/***************** Sticky aside-nav ********************/
+const wrapper = $('#wrapper');
+const contentLeftInnerOffset = $("#nav-aside").offset().top;
+const asideFixedTopClassname = 'aside-fixed--top';
+
+$(window).scroll(function() {
+	const scrolled = $(this).scrollTop();
+
+	if (scrolled > contentLeftInnerOffset)
+	{
+		wrapper.addClass('aside-fixed--top');
+	} else if (scrolled < contentLeftInnerOffset) {
+		wrapper.removeClass('aside-fixed--top');
+	}
+});
